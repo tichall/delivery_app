c46f1350de2b7721e21a1c4b467101d7fc3b7353
Lee0zoo <leeyj4515@naver.com>
2024-06-21 16:11:36 +0900

    fix : #15 리팩토링 - @Getter 제외

--- src/main/java/com/sparta/delivery_app/domain/menu/dto/request/MenuAddRequestDto.java
+++ src/main/java/com/sparta/delivery_app/domain/menu/dto/request/MenuAddRequestDto.java
@@ -4,9 +4,7 @@ package com.sparta.delivery_app.domain.menu.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
 
-@Getter
 public record MenuAddRequestDto(
         @NotNull(message = "Store ID가 입력되지 않았습니다.")
         Long storeId,
