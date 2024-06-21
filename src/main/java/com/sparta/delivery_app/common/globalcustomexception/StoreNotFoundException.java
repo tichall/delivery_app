e0381520b8eb9cf33d98b1b713d994936bb46c1e
Chanwon-Seo <scwonn60@gmail.com>
2024-06-21 23:41:32 +0900

    feat : #28/GlobalException 패키지 구조 변경

--- src/main/java/com/sparta/delivery_app/common/globalcustomexception/StoreNotFoundException.java
+++ src/main/java/com/sparta/delivery_app/common/globalcustomexception/StoreNotFoundException.java
@@ -1,6 +1,7 @@
 package com.sparta.delivery_app.common.globalcustomexception;
 
 import com.sparta.delivery_app.common.exception.errorcode.ErrorCode;
+import com.sparta.delivery_app.common.globalcustomexception.global.GlobalNotFoundException;
 
 public class StoreNotFoundException extends GlobalNotFoundException {
     public StoreNotFoundException(ErrorCode errorCode) {
