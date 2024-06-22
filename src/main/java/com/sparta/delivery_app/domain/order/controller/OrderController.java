f2e93d1e302e68b779e6fd95d31587d087a446ad
tichall <0817frog@gmail.com>
2024-06-22 19:02:29 +0900

    refactor : #68/order 시큐리티 반영 리팩토링

--- src/main/java/com/sparta/delivery_app/domain/order/controller/OrderController.java
+++ src/main/java/com/sparta/delivery_app/domain/order/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.sparta.delivery_app.domain.order.controller;
 
 import com.sparta.delivery_app.common.globalResponse.RestApiResponse;
+import com.sparta.delivery_app.common.security.AuthenticationUser;
 import com.sparta.delivery_app.common.status.StatusCode;
 import com.sparta.delivery_app.domain.order.dto.request.OrderAddRequestDto;
 import com.sparta.delivery_app.domain.order.dto.response.OrderAddResponseDto;
@@ -11,6 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -21,16 +23,22 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<RestApiResponse<OrderAddResponseDto>> orderAdd(@Valid @RequestBody final OrderAddRequestDto requestDto) {
-        OrderAddResponseDto responseDto = orderService.addOrder(requestDto);
+    public ResponseEntity<RestApiResponse<OrderAddResponseDto>> orderAdd(
+            @AuthenticationPrincipal AuthenticationUser authenticationUser,
+            @Valid @RequestBody final OrderAddRequestDto requestDto)
+    {
+        OrderAddResponseDto responseDto = orderService.addOrder(authenticationUser, requestDto);
 
         return ResponseEntity.status(StatusCode.CREATED.getCode())
                 .body(RestApiResponse.of(StatusCode.CREATED.getCode(),"주문이 완료되었습니다.", responseDto));
     }
 
     @GetMapping("/{orderId}")
-    public ResponseEntity<RestApiResponse<OrderGetResponseDto>> orderDetails(@PathVariable Long orderId) {
-        OrderGetResponseDto responseDto = orderService.findOrder(orderId);
+    public ResponseEntity<RestApiResponse<OrderGetResponseDto>> orderDetails(
+            @AuthenticationPrincipal AuthenticationUser authenticationUser,
+            @PathVariable Long orderId)
+    {
+        OrderGetResponseDto responseDto = orderService.findOrder(authenticationUser, orderId);
 
         return ResponseEntity.status(StatusCode.OK.getCode())
                 .body(RestApiResponse.of(responseDto));
@@ -38,10 +46,13 @@ public class OrderController {
 
     @GetMapping
     public ResponseEntity<RestApiResponse<OrderPageResponseDto>> orderPage(
+            @AuthenticationPrincipal AuthenticationUser authenticationUser,
             @RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum,
             @RequestParam(value = "sortBy", required = false, defaultValue = "createdAt") String sortBy,
-            @RequestParam(value = "isDesc", required = false, defaultValue = "true") Boolean isDesc) {
-        OrderPageResponseDto responseDto = orderService.findOrders(pageNum, sortBy, isDesc);
+            @RequestParam(value = "isDesc", required = false, defaultValue = "true") Boolean isDesc)
+    {
+
+        OrderPageResponseDto responseDto = orderService.findOrders(authenticationUser,pageNum, sortBy, isDesc);
 
         return ResponseEntity.status(StatusCode.OK.getCode())
                 .body(RestApiResponse.of(responseDto));
--- src/main/java/com/sparta/delivery_app/domain/order/service/OrderService.java
+++ src/main/java/com/sparta/delivery_app/domain/order/service/OrderService.java
@@ -1,10 +1,9 @@
 package com.sparta.delivery_app.domain.order.service;
 
-import com.sparta.delivery_app.common.exception.errorcode.MenuErrorCode;
 import com.sparta.delivery_app.common.exception.errorcode.OrderErrorCode;
-import com.sparta.delivery_app.common.globalcustomexception.MenuStatusException;
 import com.sparta.delivery_app.common.globalcustomexception.StoreMenuMismatchException;
 import com.sparta.delivery_app.common.globalcustomexception.TotalPriceException;
+import com.sparta.delivery_app.common.security.AuthenticationUser;
 import com.sparta.delivery_app.domain.commen.page.util.PageUtil;
 import com.sparta.delivery_app.domain.menu.adaptor.MenuAdaptor;
 import com.sparta.delivery_app.domain.menu.entity.Menu;
@@ -20,12 +19,12 @@ import com.sparta.delivery_app.domain.order.entity.OrderItem;
 import com.sparta.delivery_app.domain.order.entity.OrderStatus;
 import com.sparta.delivery_app.domain.store.adaptor.StoreAdaptor;
 import com.sparta.delivery_app.domain.store.entity.Store;
+import com.sparta.delivery_app.domain.user.adaptor.UserAdaptor;
+import com.sparta.delivery_app.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -35,15 +34,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderService {
     private OrderAdaptor orderAdaptor;
+    private UserAdaptor userAdaptor;
     private StoreAdaptor storeAdaptor;
     private MenuAdaptor menuAdaptor;
 
-    public OrderAddResponseDto addOrder(final OrderAddRequestDto requestDto) {
+    /**
+     * 주문 생성
+     * @param authenticationUser 인증된 유저 정보
+     * @param requestDto 주문 정보
+     * @return OrderAddResponseDto 생성된 주문 정보
+     */
+    public OrderAddResponseDto addOrder(AuthenticationUser authenticationUser, final OrderAddRequestDto requestDto) {
+        User user = userAdaptor.queryUserByEmail(authenticationUser.getUsername());
         Store store = storeAdaptor.queryStoreById(requestDto.storeId());
         Long totalPrice;
 
         Order currentOrder = Order.builder()
-//                .user(user)
+                .user(user)
                 .store(store)
                 .orderStatus(OrderStatus.ORDER_COMPLETED)
                 .build();
@@ -59,24 +66,47 @@ public class OrderService {
         return OrderAddResponseDto.of(currentOrder, totalPrice);
     }
 
-    public OrderGetResponseDto findOrder(Long orderId) {
-        // 해당 유저의 주문인지 검증 필요
-        Long userId = 0L; // 임시
-        Order order = orderAdaptor.queryOrderByIdAndUserID(userId, orderId);
+    /**
+     * 주문 단건 조회
+     * @param authenticationUser 인증된 유저 정보
+     * @param orderId 조회할 주문 아이디
+     * @return OrderGetResponseDto 조회된 주문 정보
+     */
+    public OrderGetResponseDto findOrder(AuthenticationUser authenticationUser, Long orderId) {
+        User user = userAdaptor.queryUserByEmail(authenticationUser.getUsername());
+        Order order = orderAdaptor.queryOrderByIdAndUserID(user.getId(), orderId);
         return OrderGetResponseDto.of(order);
     }
 
-    public OrderPageResponseDto findOrders(Integer pageNum, String sortBy, Boolean isDesc) {
-        Long userId = 0L; // 임시
+    /**
+     * 주문 전체 조회 (페이징)
+     * @param authenticationUser 인증된 유저 정보
+     * @param pageNum 접근할 페이지 번호
+     * @param sortBy 정렬 조건
+     * @param isDesc 내림차순 여부
+     * @return OrderPageResponseDto 조회된 페이지
+     */
+    public OrderPageResponseDto findOrders(
+            AuthenticationUser authenticationUser,
+            Integer pageNum,
+            String sortBy,
+            Boolean isDesc
+    ) {
+        User user = userAdaptor.queryUserByEmail(authenticationUser.getUsername());
 
         Pageable pageable = PageUtil.createPageable(pageNum, PageUtil.PAGE_SIZE_FIVE, sortBy, isDesc);
 
-        Page<Order> orderPage = orderAdaptor.queryOrdersByUserId(pageable, userId);
+        Page<Order> orderPage = orderAdaptor.queryOrdersByUserId(pageable, user.getId());
 
         PageUtil.validatePage(pageNum, orderPage);
         return OrderPageResponseDto.of(pageNum, orderPage);
     }
 
+    /**
+     * 선택한 메뉴들을 현재 주문에 추가
+     * @param currentOrder 현재 주문
+     * @param menuItemRequestDtoList 선택한 메뉴
+     */
     private void addValidatedMenuItemsToOrder(Order currentOrder, List<MenuItemRequestDto> menuItemRequestDtoList) {
         for (MenuItemRequestDto menuItemRequestDto : menuItemRequestDtoList) {
             Long menuId = menuItemRequestDto.menuId();
@@ -99,5 +129,4 @@ public class OrderService {
             currentOrder.addOrderItem(orderItem);
         }
     }
-
 }
