eb6f66ea2a41cbb6c0946621868e52c2cdbf4b08
Lee0zoo <leeyj4515@naver.com>
2024-06-22 19:33:19 +0900

    refactor : #67 메뉴 관련 기능에 security 추가 - 메뉴 관련 기능에 security 추가

--- src/main/java/com/sparta/delivery_app/common/globalcustomexception/ReviewNotFoundException.java
+++ src/main/java/com/sparta/delivery_app/common/globalcustomexception/ReviewNotFoundException.java
@@ -1,6 +1,7 @@
 package com.sparta.delivery_app.common.globalcustomexception;
 
 import com.sparta.delivery_app.common.exception.errorcode.ErrorCode;
+import com.sparta.delivery_app.common.globalcustomexception.global.GlobalNotFoundException;
 
 public class ReviewNotFoundException extends GlobalNotFoundException {
 
