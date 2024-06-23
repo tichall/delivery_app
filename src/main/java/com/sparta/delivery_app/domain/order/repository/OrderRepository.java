2172dd95a3b4ce96f71842942b79e79627662a9b
MonGroy <mongtualmr@gmail.com>
2024-06-23 19:54:24 +0900

    feat/ #56 import 와 주석 정리

--- src/main/java/com/sparta/delivery_app/domain/order/repository/OrderRepository.java
+++ src/main/java/com/sparta/delivery_app/domain/order/repository/OrderRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-import java.util.List;
-
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
