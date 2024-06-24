006a450c703b96775e0a6ea5f18ea17edce77e1c
Lee0zoo <leeyj4515@naver.com>
2024-06-24 12:11:47 +0900

    fix : #113 충돌 해결 - dev와 충돌 해결

--- src/main/java/com/sparta/delivery_app/domain/openApi/dto/ReviewPageResponseDto.java
+++ src/main/java/com/sparta/delivery_app/domain/openApi/dto/ReviewPageResponseDto.java
@@ -1,9 +1,8 @@
 package com.sparta.delivery_app.domain.openApi.dto;
 
-import com.sparta.delivery_app.domain.review.dto.response.UserReviewResponseDto;
-import com.sparta.delivery_app.domain.review.entity.ReviewStatus;
+
+import com.sparta.delivery_app.domain.review.dto.response.UserReviewAddResponseDto;
 import com.sparta.delivery_app.domain.review.entity.UserReviews;
-import com.sparta.delivery_app.domain.user.entity.UserRole;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
@@ -23,7 +22,7 @@ public class ReviewPageResponseDto {
                 .currentPage(currentPage)
                 .totalReviews(reviewPage.getTotalElements())
                 .reviewList(reviewPage.getContent().stream()
-                        .map(UserReviewResponseDto::of).toList())
+                        .map(UserReviewAddResponseDto::of).toList())
                 .build();
     }
 }
