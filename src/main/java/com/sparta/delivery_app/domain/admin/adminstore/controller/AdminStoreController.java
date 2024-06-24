3b5fcfaabad41c6b5b975d096a734374f02952ae
Lee0zoo <leeyj4515@naver.com>
2024-06-24 14:54:54 +0900

    feat : #123 특정 매장의 메뉴별 총 판매금액 조회

--- src/main/java/com/sparta/delivery_app/domain/admin/adminstore/controller/AdminStoreController.java
+++ src/main/java/com/sparta/delivery_app/domain/admin/adminstore/controller/AdminStoreController.java
@@ -4,6 +4,7 @@ import com.sparta.delivery_app.common.globalResponse.RestApiResponse;
 import com.sparta.delivery_app.common.security.AuthenticationUser;
 import com.sparta.delivery_app.common.status.StatusCode;
 import com.sparta.delivery_app.domain.admin.adminstore.dto.PageMenuPerStoreResponseDto;
+import com.sparta.delivery_app.domain.admin.adminstore.dto.PageTotalPricePerStoreResponseDto;
 import com.sparta.delivery_app.domain.admin.adminstore.dto.ReviewPerStoreResponseDto;
 import com.sparta.delivery_app.domain.admin.adminstore.service.AdminStoreService;
 import lombok.RequiredArgsConstructor;
@@ -63,6 +64,29 @@ public class AdminStoreController {
         log.info("특정 매장 모든 리뷰 조회-controller-reviewDtoList 생성 완료");
         return ResponseEntity.status(StatusCode.CREATED.code)
                 .body(RestApiResponse.of("조회 성공", reviewDtoList));
+    }
+
+    /**
+     * 특정 매장 메뉴별 총 판매 금액 조회
+     * @param storeId
+     * @param user
+     * @param pageNum
+     * @param sortBy
+     * @param isDesc
+     * @return
+     */
+    @GetMapping("/{storeId}/earning")
+    public ResponseEntity<RestApiResponse<PageTotalPricePerStoreResponseDto>> getEarning(
+            @PathVariable final Long storeId,
+            @AuthenticationPrincipal AuthenticationUser user,
+            @RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum,
+            @RequestParam(value = "sortBy", required = false, defaultValue = "menu") String sortBy,
+            @RequestParam(value = "isDesc", required = false, defaultValue = "true") Boolean isDesc
+    ) {
+        PageTotalPricePerStoreResponseDto map = adminStoreService.getEarning(pageNum, sortBy, isDesc, storeId);
+
+        return ResponseEntity.status(StatusCode.OK.code)
+                .body(RestApiResponse.of("성공", map));
     }
 
 }
